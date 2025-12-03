53daf85ebc005a624a31e8b947a56a4e94916454
rsun19 <robertssun1234@gmail.com>
2025-12-03 10:48:22 -0500

    changed conversion into function

--- mlflow/server/js/src/common/styles/patternfly/patternflyStyles/patternflyBorders.ts
+++ mlflow/server/js/src/common/styles/patternfly/patternflyStyles/patternflyBorders.ts
@@ -5,15 +5,16 @@ import {
   t_global_border_radius_large,
   t_global_border_radius_pill,
 } from '@patternfly/react-tokens';
+import { convertPxStringToPx } from '../utils';
 export const patternflyBorders = {
-  borderRadius0: Number(t_global_border_radius_sharp.value.replace('px', '')),
-  borderRadiusSm: Number(t_global_border_radius_small.value.replace('px', '')),
-  borderRadiusMd: Number(t_global_border_radius_medium.value.replace('px', '')),
-  borderRadiusLg: Number(t_global_border_radius_large.value.replace('px', '')),
-  borderRadiusFull: Number(t_global_border_radius_pill.value.replace('px', '')),
+  borderRadius0: convertPxStringToPx(t_global_border_radius_sharp.value),
+  borderRadiusSm: convertPxStringToPx(t_global_border_radius_small.value),
+  borderRadiusMd: convertPxStringToPx(t_global_border_radius_medium.value),
+  borderRadiusLg: convertPxStringToPx(t_global_border_radius_large.value),
+  borderRadiusFull: convertPxStringToPx(t_global_border_radius_pill.value),
 };
 
 export const patternflyLegacyBorders = {
-  borderRadiusMd: Number(t_global_border_radius_medium.value.replace('px', '')),
-  borderRadiusLg: Number(t_global_border_radius_large.value.replace('px', '')),
+  borderRadiusMd: convertPxStringToPx(t_global_border_radius_medium.value),
+  borderRadiusLg: convertPxStringToPx(t_global_border_radius_large.value),
 };
